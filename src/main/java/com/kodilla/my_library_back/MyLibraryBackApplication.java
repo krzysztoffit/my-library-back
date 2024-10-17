39c70d0e0167ab33a635df906831ed8c13c9bc3b
Krzysztof Fit <krzysztof.fit@hotmail.com>
2024-10-17 21:49:35 +0200

    First commit

---
+++ src/main/java/com/kodilla/my_library_back/MyLibraryBackApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.my_library_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/my_library_back/MyLibraryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.my_library_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
